11ca34dff8891d3c8889ccde9200c67d18abe177
sun <373940255>
2019-01-09 15:37:55 +0800

    增加JS 第一次

--- main.js
+++ main.js
@@ -1,2 +1,43 @@
+
 var wrapper = document.querySelector('#site-wrapper')
-wrapper.classList.remove('active')
+setTimeout(function() {
+    wrapper.classList.remove('active')
+}, 500)
+var navBar = document.querySelector('.topNavBar')
+
+window.onscroll = function() {
+    if (scrollY > 0) {
+        navBar.classList.add('background')
+    } else {
+        navBar.classList.remove('background')   
+    }
+}
+
+var navSliderA = document.querySelectorAll('#navMenu > ul > li')
+for (let index = 0; index < navSliderA.length; index++) {
+    const x = navSliderA[index]
+
+    x.addEventListener('mouseenter', function(event) {
+        var target = event.target
+        target.classList.add('active1')
+    })
+
+    x.addEventListener('mouseleave', function(event) {
+        var target = event.target
+        target.classList.remove('active1')
+    }) 
+}
+
+var navA = document.querySelectorAll('#navMenu > ul > li > a')
+
+for (let index = 0; index < navA.length; index++) {
+    const x = navA[index]
+    x.onclick = function name(e) {
+        e.preventDefault()
+        var address = x.getAttribute('href')
+        var pageto = document.querySelector(address)
+        var y = pageto.offsetTop
+        window.scrollTo(0, y - 105)
+    }
+
+}
