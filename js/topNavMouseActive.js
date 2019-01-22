1b44a16b21f3cd40f02a394d2e1e7d0b052b4932
sun <373940255>
2019-01-22 19:10:53 +0800

    使用MVC思想继续优化代码结构

--- js/topNavMouseActive.js
+++ js/topNavMouseActive.js
@@ -1,16 +1,36 @@
 !function() {
-    var navSliderA = document.querySelectorAll('#navMenu > ul > li')
-    for (let index = 0; index < navSliderA.length; index++) {
-        const x = navSliderA[index]
-
-        x.addEventListener('mouseenter', function (event) {
-            var target = event.currentTarget
+    var view = document.querySelector('#navMenu')
+    var controller = {
+        view: null,
+        liTags: null,
+        init: function(view) {
+            this.view = view
+            this.bindEvents()
+        },
+        bindEvents: function() {
+            this.liTags = this.view.querySelectorAll('ul > li')
+            for (let index = 0; index < this.liTags.length; index++) {
+                const x = this.liTags[index]
+                x.addEventListener('mouseenter', function(event) {
+                    var target = event.currentTarget
+                    this.active(target)
+                    
+                }.bind(controller))    
+                x.addEventListener('mouseleave', (event) => {
+                    var target = event.currentTarget
+                    this.deleteActive(target)
+                })
+            }
+        },
+        active: function(target) {
             target.classList.add('active1')
-        })
+        },
 
-        x.addEventListener('mouseleave', function (event) {
-            var target = event.currentTarget
+        deleteActive: function(target) {
             target.classList.remove('active1')
-        })
+        }
     }
+
+    controller.init(view)
+
 }.call()
